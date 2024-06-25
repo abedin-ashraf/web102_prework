41928b4f1147b41ce4a4f9ce3600399be0c8fa61
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2024-06-25 17:57:02 -0500

    Completed Challenge 3

--- index.js
+++ index.js
@@ -29,28 +29,36 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
         // create a new div element, which will become the game card
-
-
+        var div = document.createElement('div');
         // add the class game-card to the list
-
-
+        div.className = "game-card";
         // set the inner HTML using a template literal to display some info 
         // about each game
+        div.innerHTML =
+            `<h3> ${games[i].name} </h3>
+         <h5> ${games[i].description} </h5>
+         <img className = "game-img" src = ${games[i].img}>
+        `
+
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
 
         // append the game to the games-container
+        gamesContainer.appendChild(div);
+    }
+
+
+
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
 
-
+addGamesToPage(GAMES_JSON)
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
  * total number of contributions, amount donated, and number of games on the site.
@@ -144,7 +152,7 @@ const descriptionContainer = document.getElementById("description-container");
 const firstGameContainer = document.getElementById("first-game");
 const secondGameContainer = document.getElementById("second-game");
 
-const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
+const sortedGames = GAMES_JSON.sort((item1, item2) => {
     return item2.pledged - item1.pledged;
 });
 
